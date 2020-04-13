2fdf2701b645a0ab369db55a0047c26f3ff8591a
JACK <jack71728ck990558@gmail.com>
2020-04-13 19:49:15 +0800

    learnt alot

---
+++ hackerrank/practice/interview_preparation_kit/dictionaries_and_hashmaps/countTriplets.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+// #include <vector>
+// #include <string>
+// #include <unordered_map>
+// #include <fstream>
+// #include <iostream>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+// Complete the countTriplets function below.
+long countTriplets(vector<long> arr, long r) {
+    unordered_map<long, long> c2, c3;
+    long ans = 0;
+    for(int i = 0; i < arr.size(); ++i) {
+        long num = arr[i];
+        if(c3.find(num) != c3.end()) {
+            ans += c3[num];
+        }
+        if(c2.find(num) != c2.end()) {
+            c3[num*r] += c2[num];
+        }
+        ++c2[num*r];
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nr_temp;
+    getline(cin, nr_temp);
+
+    vector<string> nr = split(rtrim(nr_temp));
+
+    int n = stoi(nr[0]);
+
+    long r = stol(nr[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<long> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        long arr_item = stol(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    long ans = countTriplets(arr, r);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
