adc78c185cb2860cc3fbb5283146ee691769ec7c
JACK <jack71728ck990558@gmail.com>
2020-04-17 16:45:10 +0800

    added stack

--- hackerrank/practice/interview_preparation_kit/stacks_and_queues/poisonousPlants.cpp
+++ hackerrank/practice/interview_preparation_kit/stacks_and_queues/poisonousPlants.cpp
@@ -6,36 +6,56 @@ vector<string> split_string(string);
 
 // Complete the poisonousPlants function below.
 int poisonousPlants(vector<int> plants) {
-    list<list<int>> list_of_stacks;
+    stack<pair<int, int>> s;
+    int max_day = 0;
+    for(int &p : plants) {
+        // pop them
+        int days = 1;
+        while(s.size() && s.top().first >= p) {
+            days = max(days, s.top().second + 1);
+            s.pop();
+        }
+
+        if(s.empty()) {
+            s.push({p, 0});
+        }
+        else {
+            s.push({p, days});
+            max_day = max(max_day, days);
+        }
+    }
+    return max_day;
+    /*
+    list<list<int>> list_of_list;
     list<int> cur_list;
     for(int &plant : plants) {
         // maintain each stack's decreasing order
         if(cur_list.size() > 0 && cur_list.back() < plant) {
-            list_of_stacks.push_back(cur_list); // copies
+            list_of_list.push_back(cur_list); // copies
             cur_list.clear();
         }
         cur_list.push_back(plant);
     }
-    list_of_stacks.push_back(cur_list);
+    list_of_list.push_back(cur_list);
     int days = 0;
-    while(list_of_stacks.size() > 1) {
-        auto it = list_of_stacks.begin();
+    while(list_of_list.size() > 1) {
+        auto it = list_of_list.begin();
         ++it;
-        for(; it != list_of_stacks.end(); ++it) {
+        for(; it != list_of_list.end(); ++it) {
             // remove each stacks (instead of first one) front
             it->pop_front();
         }
-        it = list_of_stacks.begin();
-        while(it != list_of_stacks.end()) {
+        it = list_of_list.begin();
+        while(it != list_of_list.end()) {
             auto next_it = it;
             ++next_it;
-            if(next_it == list_of_stacks.end()) break;
+            if(next_it == list_of_list.end()) break;
             if(next_it->empty()) { // if next empty
-                list_of_stacks.erase(next_it);
+                list_of_list.erase(next_it);
             }
             else if(it->back() >= next_it->front()) { // if current tail >= next head
                 it->splice(it->end(), *next_it); // merge in O(1)
-                list_of_stacks.erase(next_it);
+                list_of_list.erase(next_it);
             }
             else {
                 ++it;
@@ -44,6 +64,7 @@ int poisonousPlants(vector<int> plants) {
         ++days;
     }
     return days;
+    */
 }
 
 int main()
