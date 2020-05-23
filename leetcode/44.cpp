ce9a81a905af97fbfc7d23030e5bc813f140df3b
JACK <jack71728ck990558@gmail.com>
2020-05-23 18:41:48 +0800

    44. Wildcard Matching

--- leetcode/44.cpp
+++ leetcode/44.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         vector<vector<bool>> dp(p.size()+1, vector<bool>(s.size()+1, false));
-
-        // s&p both empty
         dp[0][0] = true;
-
-        // p empty && s not empty, dp[0][j] remains false
-
-        // p not empty && s empty, take care of dp[i][0]
+        // dp[0][j] = false
+        // dp[i][0]
         for(int i = 1; i <= p.size(); ++i) {
-            if(p[i-1] == '*')
+            char &cur_p = p[i-1];
+            if(cur_p == '*') {
                 dp[i][0] = dp[i-1][0];
+            }
         }
 
         for(int i = 1; i <= p.size(); ++i) {
-            bool res = dp[i-1][0];
+            char &cur_p = p[i-1];
+            bool result = dp[i-1][0];
             for(int j = 1; j <= s.size(); ++j) {
-                // dp[i][j]  s[i-1]  p[j-1]
-                res = res | dp[i-1][j];
-                char &cur_p = p[i-1], &cur_s = s[j-1];
-                if(cur_p == '?') {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if(isalpha(cur_p)) {
-                    dp[i][j] = (cur_s == cur_p) && dp[i-1][j-1];
+                char &cur_s = s[j-1];
+                result = result || dp[i-1][j];
+                if(cur_p == '*') {
+                    // OR { dp[i-1][x] for x from 0 to j }
+                    dp[i][j] = result;
                 }
                 else {
-                    // *
-                    // for(int len = 0; len <= j; ++len) {
-                    //     // * matches s.substr(j-len, len)
-                    //     dp[i][j] = dp[i][j] | dp[i-1][j-len];
-                    // }
-                    dp[i][j] = res;
+                    dp[i][j] = (cur_p == '?' || (cur_p == cur_s)) && dp[i-1][j-1];
                 }
             }
         }
