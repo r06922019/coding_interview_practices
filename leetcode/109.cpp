fa886cd4b3f64da44a6fed27f80a00f8ea2aab1e
JACK <jack71728ck990558@gmail.com>
2020-06-07 16:26:03 +0800

    109. Convert Sorted List to Binary Search Tree slow & fast pointers to split list, recursive call

---
+++ leetcode/109.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head == nullptr) return nullptr;
+        if(head->next == nullptr) return new TreeNode(head->val);
+
+        // printf("calling %d\n", head->val);
+        // find mid
+        ListNode *slow = head, *fast = head, *prev = head;
+        while(fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *mid = slow;
+        prev->next = nullptr;
+        TreeNode *root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+        return root;
+    }
+};
