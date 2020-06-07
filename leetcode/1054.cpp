209ef69130cea0705d1b9a3a0682f8b1b5f9fe3f
JACK <jack71728ck990558@gmail.com>
2020-06-08 03:09:56 +0800

    1054. Distant Barcodes Organinzed! sorting with lambda

--- leetcode/1054.cpp
+++ leetcode/1054.cpp
@@ -1,36 +1,54 @@
+class CodeGroup {
+public:
+    int code, count;
+    CodeGroup() {
+        code = count = 0;
+    }
+    CodeGroup(int _code, int _count) {
+        code = _code;
+        count = _count;
+    }
+
+};
+
 class Solution {
 public:
     vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        int n = barcodes.size();
+
+        // counter
         unordered_map<int, int> counter;
-        for(int &cod : barcodes) {
-            ++counter[cod];
+        for(int &c : barcodes) {
+            ++counter[c];
         }
 
-        vector<vector<int>> code_groups;
+        // sort w.r.t occurences
+        vector<CodeGroup> code_groups;
         for(auto &it : counter) {
-            int code = it.first, count = it.second;
-            code_groups.push_back({count, code});
+            const int &code = it.first, &count = it.second;
+            code_groups.emplace_back(code, count);
         }
 
-        sort(code_groups.rbegin(), code_groups.rend());
+        // descending
+        sort(code_groups.begin(), code_groups.end(), [](const CodeGroup &a, CodeGroup &b) {
+            return a.count > b.count || (a.count == b.count && a.code > b.code);
+        });
 
         // expand
-        vector<int> sorted_with_count;
+        queue<int> sorted_with_count;
         for(auto &group : code_groups) {
-            int code = group[1], count = group[0];
+            int code = group.code, count = group.count;
             while(count--) {
-                sorted_with_count.push_back(code);
+                sorted_with_count.push(code);
             }
         }
 
-        vector<int> ans(barcodes.size(), 0);
-        int index = 0, n = barcodes.size();
-        for(int i = 0; i < n; i += 2, ++index) {
-            ans[i] = sorted_with_count[index];
+        vector<int> ans(n, 0);
+        for(int i = 0; i < n; i += 2, sorted_with_count.pop()) {
+            ans[i] = sorted_with_count.front();
         }
-
-        for(int i = 1; i < n; i += 2, ++index) {
-            ans[i] = sorted_with_count[index];
+        for(int i = 1; i < n; i += 2, sorted_with_count.pop()) {
+            ans[i] = sorted_with_count.front();
         }
         return ans;
     }
