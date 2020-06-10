f6d60c013e998aa84ac44a2b37ab4e6509797578
JACK <jack71728ck990558@gmail.com>
2020-06-11 03:09:29 +0800

    added comparison with pq

--- leetcode/451.cpp
+++ leetcode/451.cpp
@@ -1,3 +1,4 @@
+typedef pair<char, int> PCI;
 class Solution {
 public:
     string frequencySort(string s) {
@@ -6,23 +7,50 @@ public:
             ++counter[c];
         }
 
-        vector<pair<char, int>> items;
-        for(auto &it : counter) {
-            items.push_back({it.first, it.second});
-        }
-
-        sort(items.begin(), items.end(), [](const pair<char, int> &a, const pair<char, int> &b) {
-            return a.second > b.second;
-        });
-
         string ans = "";
-        for(auto &p : items) {
-            char &c = p.first;
-            int count = p.second;
-            for(int i = 0; i < count; ++i) {
-                ans += c;
+        bool use_pq = false;
+        if(use_pq) {
+            function<bool (const PCI, const PCI)> comp = [](const PCI &a, const PCI &b) {
+                // if true, b comes first;
+                return a.second < b.second;
+            };
+
+            priority_queue<PCI, vector<PCI>, decltype(comp)> pq(comp);
+
+            for(auto &it : counter) {
+                pq.push({it.first, it.second});
+            }
+
+            while(pq.size()) {
+                PCI p = pq.top();
+                pq.pop();
+                char &c = p.first;
+                int count = p.second;
+                for(int i = 0; i < count; ++i) {
+                    ans += c;
+                }
             }
         }
+        else {
+            vector<pair<char, int>> items;
+            for(auto &it : counter) {
+                items.push_back({it.first, it.second});
+            }
+
+            sort(items.begin(), items.end(), [](const PCI &a, const PCI &b) {
+                // if true, a comes first;
+                return a.second > b.second;
+            });
+
+            for(auto &p : items) {
+                char &c = p.first;
+                int count = p.second;
+                for(int i = 0; i < count; ++i) {
+                    ans += c;
+                }
+            }
+        }
+
         return ans;
     }
 };
