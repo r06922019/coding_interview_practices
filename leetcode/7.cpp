aa457486ea9f896aae31df42f778a58cc8501a0e
JACK <jack71728ck990558@gmail.com>
2020-06-15 00:57:32 +0800

    7. Reverse Integer check if x % 10 if positive or not

---
+++ leetcode/7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x) {
+            int digit = x % 10;
+            if(digit > 0) { // x is positive
+                if(ans > INT_MAX/10) return 0;
+                ans *= 10;
+                if(ans > INT_MAX - digit) return 0;
+                ans += digit;
+            }
+            else {
+                if(ans < INT_MIN/10) return 0;
+                ans *= 10;
+                if(ans < INT_MIN - digit) return 0;
+                ans += digit;
+            }
+            x /= 10;
+        }
+        return ans;
+    }
+};
