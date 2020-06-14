a81f252682231a835fd73e1c47a8250d79466ec3
JACK <jack71728ck990558@gmail.com>
2020-06-15 01:00:24 +0800

    19. Remove Nth Node From End of List slow & fast pointers

---
+++ leetcode/19.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummy = new ListNode(0, head);
+        ListNode *slow = dummy, *fast = dummy;
+        for(int i = 0; i < n; ++i)
+            fast = fast->next;
+        while(fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode *to_remove = slow->next;
+        slow->next = to_remove->next;
+        delete(to_remove);
+        return dummy->next;
+    }
+};
