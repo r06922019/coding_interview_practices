b44a8bb3f574d270e9ab2f474469774e59fef23b
JACK <jack71728ck990558@gmail.com>
2020-06-15 01:04:01 +0800

    rather straightforward

---
+++ leetcode/1147.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestDecomposition(string text) {
+        int len = text.size();
+        if(len <= 1) return len; // empty string or single char
+
+        int half_len = len / 2, ans = 1;
+        for(int prefix_len = 1; prefix_len <= half_len; ++prefix_len) {
+            if(text.substr(0, prefix_len) == text.substr(len-prefix_len, prefix_len)) {
+                ans = max(ans, 2+longestDecomposition(text.substr(prefix_len, len - prefix_len * 2)));
+            }
+        }
+        return ans;
+    }
+};
