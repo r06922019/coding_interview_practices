eed57b14e85ac0665ab52b07e29f7850fe72004d
JACK <jack71728ck990558@gmail.com>
2020-06-15 02:23:38 +0800

    hasher, dfs

--- leetcode/980.cpp
+++ leetcode/980.cpp
@@ -53,10 +53,13 @@ public:
 
 class MyHash {
 public:
+    static hash<int> hasher;
     size_t operator()(const Pos &p) const {
-        return (p.i << 16) ^ p.j;
+        return (hasher(p.i) << 16) ^ hasher(p.j);
     }
 };
+
+hash<int> MyHash::hasher = hash<int>();
 
 typedef unordered_set<Pos, MyHash> POS_SET;
 typedef unordered_map<Pos, Pos, MyHash> POS_MAP_POS;
@@ -144,3 +147,59 @@ public:
         return ;
     }
 };
+
+/*
+class Solution {
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int path_count = 0, m = grid.size(), n = grid[0].size();
+        int start_i, start_j, end_i, end_j, total_good_blocks = 0;
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j] == 1) {
+                    start_i = i;
+                    start_j = j;
+                }
+                else if(grid[i][j] == 2) {
+                    end_i = i;
+                    end_j = j;
+                }
+                else if(grid[i][j] == 0) {
+                    ++total_good_blocks;
+                }
+            }
+        }
+        ++total_good_blocks; // for S
+        dfs(grid, start_i, start_j, end_i, end_j, 0, total_good_blocks, path_count);
+        return path_count;
+    }
+
+    void dfs(vector<vector<int>>& grid, int i, int j,
+            int end_i, int end_j, int visited, int total_good_blocks,
+            int &path_count) {
+
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] == -1)
+            return ;
+
+        if(i == end_i && j == end_j) {
+            if(visited == total_good_blocks) {
+                ++path_count;
+            }
+            return ;
+        }
+
+        ++visited; // visit current
+        grid[i][j] = -1; // block current
+        dfs(grid, i-1, j, end_i, end_j, visited, total_good_blocks, path_count);
+        dfs(grid, i+1, j, end_i, end_j, visited, total_good_blocks, path_count);
+        dfs(grid, i, j-1, end_i, end_j, visited, total_good_blocks, path_count);
+        dfs(grid, i, j+1, end_i, end_j, visited, total_good_blocks, path_count);
+        grid[i][j] = 0; // unblock current
+        --visited;
+    }
+
+};
+
+*/
+
+
