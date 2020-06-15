0f787c0a440e430708f7e65f4818fa2433051a90
JACK <jack71728ck990558@gmail.com>
2020-06-16 01:15:33 +0800

    cartesian product?

---
+++ leetcode/454.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> AxB;
+        for(int i = 0; i < A.size(); ++i) {
+            for(int j = 0; j < B.size(); ++j) {
+                ++AxB[A[i] + B[j]];
+            }
+        }
+
+        int ans = 0;
+        for(int k = 0; k < C.size(); ++k) {
+            for(int l = 0; l < D.size(); ++l) {
+                int target = 0 - (C[k] + D[l]);
+                if(AxB.find(target) != AxB.end())
+                    ans += AxB[target];
+            }
+        }
+        return ans;
+    }
+};
