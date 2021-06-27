a31ae8b6645c3912dbf5016c2177e0a98a8385b1
jack <jack@mac_mini>
2021-06-27 17:48:18 +0800

    q

---
+++ templates/priority_q/priority_queue_with_lambda.cpp
@@ -0,0 +1,17 @@
+#include <queue>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+#define T int
+
+int main() {
+    auto comp = [](const T &a, const T &b) -> bool { return a > b; };
+    priority_queue<T, vector<T>, decltype(comp)> pq(comp);
+    for(int i = 0; i < 3; ++i) {
+        pq.push(i);
+    }
+    printf("%d\n", pq.top());
+    return 0;
+}
