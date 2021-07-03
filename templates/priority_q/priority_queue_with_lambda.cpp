910efc98c929ec6b45685bb9ea1de2cb58fb2679
jack <jack@mac_mini>
2021-07-03 16:49:58 +0800

    with lambda

--- templates/priority_q/priority_queue_with_lambda.cpp
+++ templates/priority_q/priority_queue_with_lambda.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define T int
 
 int main() {
-    auto comp = [](const T &a, const T &b) -> bool { return a > b; };
+    auto comp = [](const T &a, const T &b) -> bool { return a > b; }; // min_queue
     priority_queue<T, vector<T>, decltype(comp)> pq(comp);
     for(int i = 0; i < 3; ++i) {
         pq.push(i);
