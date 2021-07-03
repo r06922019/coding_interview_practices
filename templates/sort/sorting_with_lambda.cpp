d7bb598670be754e56751b4d7f815b2c958d105c
jack <jack@mac_mini>
2021-07-03 17:50:49 +0800

    lambda

---
+++ templates/sort/sorting_with_lambda.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        int n = votes[0].size();
+        vector<vector<int>> results(26, vector<int>(n, 0));
+        vector<bool> seen(26, false);
+        for(string &vote : votes) {
+            for(int i = 0; i < n; ++i) {
+                char &c = vote[i];
+                int index = c - 'A';
+                seen[index] = true;
+                ++results[index][i];
+            }
+        }
+        vector<char> ans;
+        for(int i = 0; i < 26; ++i) {
+            if(seen[i]) {
+                ans.push_back('A' + i);
+            }
+        }
+        auto comp = [&results, &n](const char &a, const char &b) { // a in front of b?
+            for(int i = 0; i < n; ++i) {
+                if(results[a-'A'][i] > results[b-'A'][i]) return true; // a will be in front of b
+                if(results[a-'A'][i] < results[b-'A'][i]) return false;
+            }
+            return a < b;
+        };
+        sort(ans.begin(), ans.end(), comp);
+        return string(ans.begin(), ans.end());
+    }
+};
