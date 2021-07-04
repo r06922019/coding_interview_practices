d3bba696f3b1a5a23209a18f3666b81913714860
jack <jack@mac_mini>
2021-07-04 17:52:50 +0800

    iterative, constant space

--- leetcode/138.cpp
+++ leetcode/138.cpp
@@ -14,6 +14,40 @@ public:
 };
 */
 
+// iterative, constant space
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node *cur = head;
+        while(cur) {
+            Node *next = cur->next;
+            cur->next = new Node(cur->val);
+            cur->next->next = next;
+            cur = next;
+        }
+
+        cur = head;
+        while(cur) {
+            if(cur->random)
+                cur->next->random = cur->random->next;
+            cur = cur->next->next;
+        }
+
+        Node *dummy = new Node(-1);
+        Node *new_cur = dummy;
+        cur = head;
+        while(cur) {
+            new_cur->next = cur->next;
+            cur->next = cur->next->next;
+            cur = cur->next;
+            new_cur = new_cur->next;
+        }
+        Node *new_head = dummy->next;
+        delete dummy;
+        return new_head;
+    }
+};
+
 class Solution {
 public:
     unordered_map<Node *, Node *> node_mapping;
