5376cf8a4efce11f9526025eeb6d3d51e3f151f9
jack <jack@mac_mini>
2021-07-16 02:22:32 +0800

    should add 1 from the neighbors

--- leetcode/542.cpp
+++ leetcode/542.cpp
@@ -5,16 +5,16 @@ public:
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
                 if(mat[i][j]) {
-                    if(j-1 >= 0) mat[i][j] = min(mat[i][j], mat[i][j-1]) + 1;
-                    if(i-1 >= 0) mat[i][j] = min(mat[i][j], mat[i-1][j]) + 1;
+                    if(j-1 >= 0) mat[i][j] = min(mat[i][j], mat[i][j-1] + 1);
+                    if(i-1 >= 0) mat[i][j] = min(mat[i][j], mat[i-1][j] + 1);
                 }
             }
         }
         for(int i = m-1; i >= 0; --i) {
             for(int j = n-1; j >= 0; --j) {
                 if(mat[i][j]) {
-                    if(j+1 < n) mat[i][j] = min(mat[i][j], mat[i][j+1]) + 1;
-                    if(i+1 < m) mat[i][j] = min(mat[i][j], mat[i+1][j]) + 1;
+                    if(j+1 < n) mat[i][j] = min(mat[i][j], mat[i][j+1] + 1);
+                    if(i+1 < m) mat[i][j] = min(mat[i][j], mat[i+1][j] + 1);
                 }
             }
         }
