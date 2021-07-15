af1b65cea4c3e18d722587adeb3c23bb034817cf
jack <jack@mac_mini>
2021-07-16 02:24:14 +0800

    should check if source is INT_MAX, if yes then ignore it(since it hasn't found its zero yet

--- leetcode/542.cpp
+++ leetcode/542.cpp
@@ -6,16 +6,20 @@ public:
             for(int j = 0; j < n; ++j) {
                 if(mat[i][j]) {
                     mat[i][j] = INT_MAX;
-                    if(j-1 >= 0) mat[i][j] = min(mat[i][j], mat[i][j-1] + 1);
-                    if(i-1 >= 0) mat[i][j] = min(mat[i][j], mat[i-1][j] + 1);
+                    if(j-1 >= 0 && mat[i][j-1] < INT_MAX)
+                        mat[i][j] = min(mat[i][j], mat[i][j-1] + 1);
+                    if(i-1 >= 0 && mat[i-1][j] < INT_MAX)
+                        mat[i][j] = min(mat[i][j], mat[i-1][j] + 1);
                 }
             }
         }
         for(int i = m-1; i >= 0; --i) {
             for(int j = n-1; j >= 0; --j) {
                 if(mat[i][j]) {
-                    if(j+1 < n) mat[i][j] = min(mat[i][j], mat[i][j+1] + 1);
-                    if(i+1 < m) mat[i][j] = min(mat[i][j], mat[i+1][j] + 1);
+                    if(j+1 < n && mat[i][j+1] < INT_MAX)
+                        mat[i][j] = min(mat[i][j], mat[i][j+1] + 1);
+                    if(i+1 < m && mat[i+1][j] < INT_MAX)
+                        mat[i][j] = min(mat[i][j], mat[i+1][j] + 1);
                 }
             }
         }
