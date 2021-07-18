168588252e616edc4f18a5fe95e85d8b7b333144
jack <jack@mac_mini>
2021-07-19 00:48:17 +0800

    dijkstra

--- codesignal/interview_practice/dfs-bfs/graphDistances.cpp
+++ codesignal/interview_practice/dfs-bfs/graphDistances.cpp
@@ -1,34 +1,31 @@
-std::vector<int> graphDistances(std::vector<std::vector<int>> g, int s) {
+vector<int> graphDistances(vector<vector<int>> g, int s) {
     int n = g.size();
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
-            if(g[i][j] < 0)
-                g[i][j] = 123456;
+            if(g[i][j] < 0) {
+                g[i][j] = 300;
+            }
         }
     }
-
-    vector<int> ans(g[s]);
+    vector<int> ans = g[s];
+    unordered_set<int> seen = {s};
     ans[s] = 0;
-    int num_nodes_left = n-1;
-    vector<bool> added(n, false);
-    added[s] = true;
-    while(num_nodes_left) {
-        int min_index = -1;
-        for(int i = 0; i < n; ++i) {
-            if(!added[i] && i != s) {
-                if(min_index < 0 || ans[min_index] > ans[i]) {
-                    min_index = i;
+    while(seen.size() < n) {
+        int min_target = -1;
+        for(int target = 0; target < n; ++target) {
+            if(seen.find(target) == seen.end()) {
+                if(min_target == -1 || ans[target] < ans[min_target]) {
+                    min_target = target;
                 }
             }
         }
-        added[min_index] = true;
-        for(int j = 0; j < n; ++j) {
-            if(!added[j] && ans[min_index] + g[min_index][j] < ans[j]) {
-                ans[j] = ans[min_index] + g[min_index][j];
+        seen.insert(min_target);
+        for(int next = 0; next < n; ++next) {
+            if(seen.find(next) == seen.end()) {
+                ans[next] = min(ans[next],
+                                ans[min_target] + g[min_target][next]);
             }
         }
-        --num_nodes_left;
     }
-
     return ans;
 }
