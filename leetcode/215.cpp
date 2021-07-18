eb1d5fbeeb6b3842806b9efb5e37392dff1feb8c
jack <jack@mac_mini>
2021-07-18 22:09:40 +0800

    min pq uses greater as comp

---
+++ leetcode/215.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        auto comp = [](const int a, const int b) { return a > b; };
+        priority_queue<int, vector<int>, decltype(comp)> pq(comp); // min pq uses greater comp
+        for(auto &num : nums) {
+            pq.push(num);
+            while(pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
--- leetcode/703.cpp
+++ leetcode/703.cpp
@@ -1,21 +1,20 @@
 class KthLargest {
 public:
     static constexpr auto comp = [](const int a, const int b) { return a > b; };
-    priority_queue<int, vector<int>, decltype(comp)> *pq;
+    priority_queue<int, vector<int>, decltype(comp)> pq;
     int k;
-    KthLargest(int _k, vector<int>& nums) {
-        pq = new priority_queue<int, vector<int>, decltype(comp)>(comp);
-        k = _k;
+
+    KthLargest(int _k, vector<int>& nums) : pq(comp), k(_k) {
         for(auto &num : nums) {
-            pq->push(num);
-            if(pq->size() > k) pq->pop();
+            pq.push(num);
+            if(pq.size() > k) pq.pop();
         }
     }
 
     int add(int val) {
-        pq->push(val);
-        if(pq->size() > k) pq->pop();
-        return pq->top();
+        pq.push(val);
+        if(pq.size() > k) pq.pop();
+        return pq.top();
     }
 };
 
