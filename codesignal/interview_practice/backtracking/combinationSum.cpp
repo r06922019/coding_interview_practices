e8f308be79ff5d371092af4a08984a4e22470212
jack <jack@mac_mini>
2021-07-19 02:41:13 +0800

    filter and sort the array to avoid repeated answers backtracking procedure favoring current value can do

--- codesignal/interview_practice/backtracking/combinationSum.cpp
+++ codesignal/interview_practice/backtracking/combinationSum.cpp
@@ -1,38 +1,29 @@
-std::string combinationSum(std::vector<int> arr, int target_sum) {
-    vector<int> buf;
-    vector<vector<int>> ans;
-    sort(arr.begin(), arr.end());
-
-    vector<int> unique_arr;
-    for(int &num : arr) {
-        if(unique_arr.empty() || num != unique_arr.back()) {
-            unique_arr.push_back(num);
+void helper(int index, vector<int> &arr, int sum, vector<int> &buf, string &ans) {
+    if(sum <= 0 || index >= arr.size()) {
+        if(sum == 0) {
+            string tmp = to_string(buf[0]);
+            for(int i = 1; i < buf.size(); ++i) {
+                tmp += " " + to_string(buf[i]);
+            }
+            ans += "(" + tmp + ")";
         }
-    }
-
-    helper(0, unique_arr, target_sum, buf, ans);
-    if(ans.size() == 0) return "Empty";
-    string ret = "";
-    for(auto &v : ans) {
-        ret += "(" + to_string(v[0]);
-        for(int i = 1; i < v.size(); ++i)
-            ret += " " + to_string(v[i]);
-        ret += ")";
-    }
-    return ret;
-}
-
-void helper(int i_start, vector<int> &arr, int target_sum, vector<int> &buf, vector<vector<int>> &ans) {
-    if(target_sum == 0) {
-        ans.push_back(buf);
         return ;
     }
 
-    for(int i = i_start; i < arr.size() && arr[i] <= target_sum; ++i) {
+    for(int i = index; i < arr.size() && arr[i] <= sum; ++i) {
         buf.push_back(arr[i]);
-        helper(i, arr, target_sum - arr[i], buf, ans);
+        helper(i, arr, sum-arr[i], buf, ans);
         buf.pop_back();
     }
-    return ;
 }
 
+string combinationSum(vector<int> arr, int sum) {
+    set<int> ans_set(arr.begin(), arr.end());
+    arr = vector<int>(ans_set.begin(), ans_set.end());
+    vector<int> buf;
+    string ans = "";
+    helper(0, arr, sum, buf, ans);
+    if(ans.empty())
+        ans = "Empty";
+    return ans;
+}
