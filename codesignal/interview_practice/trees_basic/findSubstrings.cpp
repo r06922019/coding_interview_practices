0f26a0c2fae2756fe501d18af21f3c5fa4a97e1d
jack <jack@mac_mini>
2021-07-22 02:35:22 +0800

    refresh

--- codesignal/interview_practice/trees_basic/findSubstrings.cpp
+++ codesignal/interview_practice/trees_basic/findSubstrings.cpp
@@ -1,79 +1,76 @@
-class Node {
+class TrieNode {
 public:
-    int ends = 0;
-    Node *children[52];
     int count = 0;
-
-    Node() {
-        for(int i = 0; i < 52; ++i)
-            children[i] = NULL;
-    }
-
+    unordered_map<char, TrieNode *> children;
 };
 
-int char_to_index(char c) {
-    if(isupper(c)) {
-        return 26 + c - 'A';
+class Trie {
+public:
+    TrieNode *root = nullptr;
+    Trie() {
+        root = new TrieNode();
     }
-    return c - 'a';
-}
 
-void insert(Node *cur, string s) {
-    ++cur->count;
-    for(int i = 0; i < s.size(); ++i) {
-        int index = char_to_index(s[i]);
-        if(cur->children[index] == NULL) {
-            cur->children[index] = new Node();
+    void insert(string &word) {
+        TrieNode *cur = root;
+        for(auto &c : word) {
+            if(cur->children.find(c) == cur->children.end()) {
+                cur->children[c] = new TrieNode();
+            }
+            cur = cur->children[c];
         }
-        cur = cur->children[index];
         ++cur->count;
     }
-    ++cur->ends;
-    return ;
-}
 
-bool query(Node *cur, string s) {
-    for(int i = 0; i < s.size(); ++i) {
-        int index = char_to_index(s[i]);
-        if(cur->children[index] == NULL || cur->children[index]->count == 0) {
-            return false;
+    bool search(string &word) {
+        TrieNode *cur = root;
+        for(auto &c : word) {
+            if(cur->children.find(c) == cur->children.end()) {
+                return false;
+            }
+            cur = cur->children[c];
         }
-        cur = cur->children[index];
+        return cur->count > 0;
     }
-    return cur->ends > 0;
-}
 
-std::vector<std::string> findSubstrings(std::vector<std::string> words, std::vector<std::string> parts) {
+    int search_until_first_unmatched(string &word, int start_index) {
+        TrieNode *cur = root;
+        int ret = -1;
+        for(int i = start_index; i < word.size(); ++i) {
+            auto &c = word[i];
+            if(cur->count > 0)
+                ret = max(ret, i);
+            if(cur->children.find(c) == cur->children.end()) {
+                return ret;
+            }
+            cur = cur->children[c];
+        }
+        if(cur->count > 0)
+            ret = max(ret, (int)word.size());
+        return ret;
+    }
+};
+
+vector<string> findSubstrings(vector<string> words, vector<string> parts) {
+    Trie trie;
+    for(auto &part : parts) {
+        trie.insert(part);
+    }
+
     vector<string> ans;
-
-    Node *root = new Node();
-    for(auto &p : parts) {
-        insert(root, p);
-    }
-
     for(auto &word : words) {
-        int max_index = -1, max_len = 0;
-        // try each index
+        int max_len = 0, max_len_index = -1;
         for(int i = 0; i < word.size(); ++i) {
-            int index = char_to_index(word[i]);
-            if(root->children[index] != NULL) {
-                for(int len = 1; len <= 5 && i+len <= word.size(); ++len) {
-                    if(query(root, word.substr(i, len))) {
-                        if(len > max_len) {
-                            max_len = len;
-                            max_index = i;
-                        }
-                    }
-                }
+            int end = trie.search_until_first_unmatched(word, i);
+            if(end - i > max_len) {
+                max_len = end-i;
+                max_len_index = i;
             }
         }
-        // construct new string
-        if(max_len > 0) {
-            string new_word = (max_index == 0)? "" : word.substr(0, max_index);
-            new_word += "[" + word.substr(max_index, max_len) + "]";
-            if(max_len < word.size())
-                new_word += word.substr(max_index+max_len);
-            ans.push_back(new_word);
+        if(max_len_index >= 0) {
+            ans.push_back(word.substr(0, max_len_index) +
+            "[" + word.substr(max_len_index, max_len) + "]" +
+            word.substr(max_len_index + max_len));
         }
         else {
             ans.push_back(word);
