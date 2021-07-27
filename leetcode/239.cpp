153a6f84adea9eadab9c6a639b6768163c5c8f47
jack <jack@mac_mini>
2021-07-27 23:41:33 +0800

    refresh, monotonic deque

--- leetcode/239.cpp
+++ leetcode/239.cpp
@@ -1,27 +1,48 @@
 class Item {
 public:
-    int index, value;
-    Item(int _i, int _v) {
+    int index, num;
+    Item(int _i, int _n) {
         index = _i;
-        value = _v;
+        num = _n;
+    }
+};
+
+class MyHash {
+public:
+    size_t operator()(const Item &i) {
+        return i.index ^ i.num;
     }
 };
 
 class Solution {
 public:
+    void push_num_into_deque(deque<Item> &dq, Item it) {
+        while(dq.size() && dq.back().num < it.num) {
+            dq.pop_back();
+        }
+        dq.push_back(it);
+    }
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        unordered_set<Item, MyHash> sssssssss;
+        unordered_set<Item, int, MyHash> ssssssdddddsss;
+        auto comp = [](const Item &a, const Item &b) {return a.num > b.num;};
+        priority_queue<Item, vector<Item>, decltype(comp)> pq(comp);
+
         vector<int> ans;
-        deque<Item> deq;
-        for(int i = 0; i < nums.size(); ++i) {
-            while(deq.size() && deq.front().index + k <= i) { // pop 0 when got 3
-                deq.pop_front();
+        deque<Item> dq;
+        int n = nums.size();
+        for(int i = 0; i < k; ++i) {
+            push_num_into_deque(dq, Item(i, nums[i]));
+        }
+        // get ans
+        ans.push_back(dq.front().num);
+        for(int i = k; i < n; ++i) {
+            if(dq.front().index <= i-k) {
+                dq.pop_front();
             }
-            while(deq.size() && deq.back().value < nums[i]) {
-                deq.pop_back();
-            }
-            deq.emplace_back(i, nums[i]);
-            if(i >= k-1)
-                ans.push_back(deq.front().value);
+            push_num_into_deque(dq, Item(i, nums[i]));
+            ans.push_back(dq.front().num);
         }
         return ans;
     }
