1f6fe58bbafa16d2dd1c9cfc1ac4e16af4da9660
jack <jack@mac_mini>
2021-07-29 23:59:56 +0800

    84. Largest Rectangle in Histogram
    Maintain a monotonic increasing stack, since if we have a decreasing sequence
    the larger one can be popped since it cannot participate in the later pairs

--- leetcode/84.cpp
+++ leetcode/84.cpp
@@ -1,19 +1,29 @@
+class Item {
+public:
+    int index, height;
+    Item(int _i, int _h) {
+        index = _i;
+        height = _h;
+    }
+};
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<pair<int, int>> h_stack;
+        stack<Item> s;
         heights.push_back(0);
-
-        int ans = 0;
-        for(int i = 0; i < heights.size(); ++i) {
-            int &h = heights[i], index = i;
-            while(h_stack.size() && h < h_stack.top().second) {
-                pair<int, int> top = h_stack.top();
-                h_stack.pop();
-                ans = max(ans, (i-top.first) * top.second);
-                index = top.first;
+        int n = heights.size(), ans = 0;
+        for(int i = 0; i < n; ++i) {
+            auto &h = heights[i];
+            int index = i;
+            while(s.size() && s.top().height >= h) {
+                int prev_index = s.top().index;
+                index = prev_index;
+                int height = s.top().height;
+                ans = max(ans, (i - prev_index) * height);
+                s.pop();
             }
-            h_stack.push({index, h});
+            s.push(Item(index, h));
         }
         return ans;
     }
