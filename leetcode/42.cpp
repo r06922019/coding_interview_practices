8564c20c817a4bb2b8a1e38f9ca8d15354398564
jack <jack@mac_mini>
2021-07-30 17:25:32 +0800

    42. Trapping Rain Water
    for each index, we find the left_max and right max
    the index's height will be min(left_max, right_max)
    aggregate them

--- leetcode/42.cpp
+++ leetcode/42.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int trap(vector<int>& heights) {
-        // find left right max
-        int cur_max = 0, n = heights.size();
-        vector<int> left, right(n, 0);
-        for(int &h : heights) {
-            left.push_back(cur_max);
-            cur_max = max(cur_max, h);
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> left_max = height;
+        for(int i = 1; i < n; ++i) {
+            left_max[i] = max(left_max[i], left_max[i-1]);
         }
-
-        cur_max = 0;
-        for(int i = n-1; i>= 0; --i) {
-            right[i] = cur_max;
-            cur_max = max(cur_max, heights[i]);
+        vector<int> right_max = height;
+        for(int i = n-2; i >= 0; --i) {
+            right_max[i] = max(right_max[i], right_max[i+1]);
         }
-
         int ans = 0;
-        for(int i = 0; i < n; ++i) {
-            ans += max(0, min(left[i], right[i]) - heights[i]);
+        for(int i = 1; i < n-1; ++i) {
+            ans += min(left_max[i], right_max[i]) - height[i];
         }
         return ans;
     }
