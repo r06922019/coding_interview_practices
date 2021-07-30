383227eafce6d628cd91e2273dc9bf3f04458321
jack <jack@mac_mini>
2021-07-31 01:22:41 +0800

    140. Word Break II
    Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
    
    Note that the same word in the dictionary may be reused multiple times in the segmentation.
    
    Standard dfs + backtracking

--- leetcode/140.cpp
+++ leetcode/140.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> word_set(wordDict.begin(), wordDict.end());
-        unordered_map<int, bool> cache;
-        return helper(s, 0, word_set, cache);
-    }
+    void helper(string &s, int index,
+               unordered_set<string> &word_set, int max_len,
+               vector<string> &buf, vector<string> &ans) {
+        if(index >= s.size()) {
+            if(index == s.size()) {
+                string tmp = "";
+                for(auto &token : buf) tmp += token + " ";
+                tmp.pop_back();
+                ans.push_back(tmp);
+            }
+            return ;
+        }
 
-    bool helper(string &s, int start, unordered_set<string> &word_set, unordered_map<int, bool> &cache) {
-        if(start > s.size()) return false;
-        if(start == s.size()) return true;
-
-        if(cache.find(start) != cache.end()) return cache[start];
-        bool ret = false;
-        for(auto &word : word_set) {
-            if(start + word.size() <= s.size()) {
-                if(word == s.substr(start, word.size())) {
-                    ret = ret || helper(s, start + word.size(), word_set, cache);
-                }
+        for(int len = 1; len <= max_len && index + len <= s.size(); ++len) {
+            if(word_set.find(s.substr(index, len)) != word_set.end()) {
+                buf.push_back(s.substr(index, len));
+                helper(s, index+len, word_set, max_len, buf, ans);
+                buf.pop_back();
             }
         }
-        cache[start] = ret;
-        return cache[start];
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> word_set(wordDict.begin(), wordDict.end());
+        vector<string> ans;
+        vector<string> buf;
+        int max_len = 0;
+        for(auto &word : wordDict) max_len = max(max_len, (int)word.size());
+        helper(s, 0, word_set, max_len, buf, ans);
+        return ans;
     }
 };
