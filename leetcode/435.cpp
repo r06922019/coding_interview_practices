085130c2fdfdf1fb0631ea53d1f2226a4a18f680
jack <jack@mac_mini>
2021-07-31 18:54:46 +0800

    435.cpp

---
+++ leetcode/435.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b) {
+            return a[1] < b[1];
+        });
+        int cnt = 0, cur_end = INT_MIN;
+        for(auto &interval : intervals) {
+            if(cur_end <= interval[0]) {
+                ++cnt;
+                cur_end = interval[1];
+            }
+        }
+        return intervals.size() - cnt;
+    }
+};
