c2527c4241f3e381cf8d3da8ef0e8ceb6ebbdfe4
jack <jack@mac_mini>
2021-07-31 16:19:43 +0800

    1011. Capacity To Ship Packages Within D Days
    if ship == 0 then we never make it
    if ship == inf, then we can always make it
    
    obviously there's a monotonic relation here
    so we apply binary search to get the exact ship

---
+++ leetcode/1011.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int check_days(vector<int>& weights, int ship) {
+        int days = 1, cur_ship = 0;
+        for(auto &w : weights) {
+            if(cur_ship + w > ship) {
+                ++days;
+                cur_ship = 0;
+            }
+            cur_ship += w;
+        }
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = *max_element(weights.begin(), weights.end());
+        int r = accumulate(weights.begin(), weights.end(), 0);
+        while(l < r) {
+            int mid = l + (r-l)/2;
+            if(check_days(weights, mid) <= days) {
+                r = mid;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+};
