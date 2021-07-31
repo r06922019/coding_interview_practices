2d3b821d12cc27db8b05adef28222cb5db761d5d
jack <jack@mac_mini>
2021-07-31 15:41:23 +0800

    130. Surrounded Regions
    DFS from balid borders

--- leetcode/130.cpp
+++ leetcode/130.cpp
@@ -1,122 +1,45 @@
-class Pos {
-public:
-    int i, j;
-    Pos() {
-        i = j = 0;
-    }
-    Pos(int _i, int _j) {
-        i = _i;
-        j = _j;
-    }
-    Pos(vector<int> &v) {
-        assert(v.size() == 2);
-        i = v[0];
-        j = v[1];
-    }
-
-    bool operator==(const Pos &other) const {
-        return this->i == other.i && this->j == other.j;
-    }
-
-    Pos add_vec(vector<int> &v) {
-        Pos p = *this;
-        p.i += v[0];
-        p.j += v[1];
-        return p;
-    }
-
-    bool is_valid(vector<vector<char>> &board, char val) {
-        return this->i >= 0 && this->i < board.size()
-            && this->j >= 0 && this->j < board[this->i].size()
-            && board[this->i][this->j] == val;
-    }
-
-    vector<Pos> get_valid_neighbors(vector<vector<char>> &board) {
-        static vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        vector<Pos> ret;
-        for(auto &dir : dirs) {
-            Pos cur = this->add_vec(dir);
-            if(cur.is_valid(board, 'O')) {
-                ret.push_back(cur);
-            }
-        }
-        return ret;
-    }
-};
-
-class MyHash {
-public:
-    size_t operator()(const Pos &p) const {
-        int i = (p.i >= 0)? p.i : p.i^-1;
-        int j = (p.j >= 0)? p.j : p.j^-1;
-        return (i << 16) ^ j;
-    }
-};
-
-typedef unordered_set<Pos, MyHash> POS_SET;
-
 class Solution {
 public:
-
-    void bfs(vector<vector<char>>& board, Pos start, POS_SET &visited) {
-        queue<Pos> q;
-        q.push(start);
-        visited.insert(start);
-
-        while(q.size()) {
-            Pos cur = q.front();
-            q.pop();
-            for(Pos &n : cur.get_valid_neighbors(board)) {
-                if(visited.find(n) == visited.end()) {
-                    q.push(n);
-                    visited.insert(n);
-                }
-            }
-        }
-
-        return ;
-    }
-
-    char get_value(vector<vector<char>> &board, Pos &p) {
-        return board[p.i][p.j];
-    }
-
-    void assign_value(vector<vector<char>>& board, Pos &p, char value) {
-        board[p.i][p.j] = value;
+    bool is_valid(vector<vector<char>>& board, int i, int j) {
+        return i >= 0 && i < board.size() && j >= 0 && j < board[i].size();
     }
 
     void solve(vector<vector<char>>& board) {
-        if(board.empty() || board.front().empty()) return;
-        int M = board.size(), N = board.front().size();
-        POS_SET visited;
-
-        Pos tmp(0,0);
-        for(auto &i : {0, M-1}) {
-            tmp.i = i;
-            for(int j = 0; j < N; ++j) {
-                tmp.j = j;
-                if(get_value(board, tmp) == 'O') {
-                    bfs(board, Pos(i, j), visited);
+        stack<vector<int>> st;
+        int m = board.size(), n = board[0].size();
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(i == 0 || j == 0 || i == m-1 || j == n-1) {
+                    if(board[i][j] == 'O') {
+                        st.push({i, j});
+                        board[i][j] = '.';
+                    }
+                }
+            }
+        }
+        vector<vector<int>> dirs = {
+            {0,1}, {0, -1}, {1, 0}, {-1, 0}
+        };
+        while(st.size()) {
+            auto cur = st.top();
+            st.pop();
+            for(auto &dir : dirs) {
+                int new_i = cur[0] + dir[0];
+                int new_j = cur[1] + dir[1];
+                if(is_valid(board, new_i, new_j) && board[new_i][new_j] == 'O') {
+                    board[new_i][new_j] = '.';
+                    st.push({new_i, new_j});
                 }
             }
         }
 
-        for(int i = 0; i < M; ++i) {
-            tmp.i = i;
-            for(auto &j : {0, N-1}) {
-                tmp.j = j;
-                if(get_value(board, tmp) == 'O') {
-                    bfs(board, Pos(i, j), visited);
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(board[i][j] == '.') {
+                    board[i][j] = 'O';
                 }
-            }
-        }
-
-        for(int i = 0; i < M; ++i) {
-            tmp.i = i;
-            for(int j = 0; j < N; ++j) {
-                tmp.j = j;
-                if(get_value(board, tmp) == 'O' && visited.find(tmp) == visited.end()) {
-                    assign_value(board, tmp, 'X');
+                else {
+                    board[i][j] = 'X';
                 }
             }
         }
