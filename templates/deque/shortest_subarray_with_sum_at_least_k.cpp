69762d6243386e4fbb9299092355826e97915186
jack <jack@mac_mini>
2021-07-31 19:21:53 +0800

    862. Shortest Subarray with Sum at Least K
    obviously we prefer to keep items with smaller value and
    a larger index to be our left candidate
    if we have {index1, value1} and {index2, value2}
    where index1 < index2, say we have a right side candidate
    satisfying the >= k condition wth both options,
    the latter is always better since index2 > index1
    so we can simply pop the first item safely
    
    this leads to a monotonic queue direction
    where in the queue, the values are increasing
    
    and for each potential right side
    we check from the front of the queue,
    we record the min len and pop_front until it's empty or the front
    no longer satisfy the condition ( >= k)
    
    the reason why we can safely pop front is because
    say we have 2 elements in the front, {index1, value1} and {index2, value2}
    with index1 < index2 and value1 < value2
    for these items, the current item is the first one to satisfy the condition
    if there is a righter one that satisfy the condition
    the range will be always smaller since it's righter

---
+++ templates/deque/shortest_subarray_with_sum_at_least_k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long> acc = {0};
+        for(auto &num : nums) {
+            acc.push_back(acc.back() + num);
+        }
+
+        int min_len = INT_MAX;
+
+        // O(N^2)
+        // for(int i = 0; i < n; ++i) {
+        //     for(int j = i+1; j <= n; ++j) {
+        //         if(acc[j] - acc[i] >= k) {
+        //             if(j-i < min_len) {
+        //                 min_len = j-i;
+        //             }
+        //         }
+        //     }
+        // }
+
+        deque<pair<int, int>> deq;
+        for(int i = 0; i <= n; ++i) {
+            auto num = acc[i];
+            while(deq.size() && (num - deq.front().second) >= k) {
+                min_len = min(min_len, i-deq.front().first);
+                deq.pop_front();
+            }
+            while(deq.size() && deq.back().second >= num) {
+                deq.pop_back();
+            }
+            deq.push_back({i, num});
+        }
+
+        if(min_len == INT_MAX) return -1;
+        return min_len;
+    }
+};
