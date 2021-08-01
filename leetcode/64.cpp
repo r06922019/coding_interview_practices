12be40e4ea298650973d29d2707e99783577585d
jack <jack@mac_mini>
2021-08-01 19:01:58 +0800

    64. min path sum
    we store the cost to 0,0 in each cell
    for each cell, we have two possible parents left or right
    so we choose the min one and propagate to the last one

--- leetcode/64.cpp
+++ leetcode/64.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum(vector<vector<int>> grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
-                int &cur = grid[i][j];
-                if(i == 0 && j == 0)
-                    dp[i][j] = cur;
-                else if(i == 0)
-                    dp[i][j] = dp[i][j-1] + cur;
-                else if(j == 0)
-                    dp[i][j] = dp[i-1][j] + cur;
-                else
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + cur;
+                if(i > 0 && j > 0) {
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+                }
+                else if(i > 0) {
+                    grid[i][j] += grid[i-1][j];
+                }
+                else if(j > 0) {
+                    grid[i][j] += grid[i][j-1];
+                }
             }
         }
-        return dp[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
