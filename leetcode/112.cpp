23ee086b81ce0a3ecbff33a9af8e0f1e9d470ccc
jack <jack@mac_mini>
2021-08-01 22:24:39 +0800

    112. Path Sum
    Traverse from root and check at leaf

---
+++ leetcode/112.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+        targetSum -= root->val;
+        if(root->left == nullptr && root->right == nullptr) {
+            return targetSum == 0;
+        }
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+};
