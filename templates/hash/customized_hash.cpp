c91177bdeade4333545c3f8ad3a6cf004fce9a61
jack <jack@mac_mini>
2021-08-02 00:15:28 +0800

    Hash for pair

---
+++ templates/hash/customized_hash.cpp
@@ -0,0 +1,9 @@
+class PairHash {
+public:
+    size_t operator()(const pair<int, int> &p) const {
+        return p.first ^ p.second;
+    }
+};
+
+typedef unordered_set<pair<int, int>, PairHash> PAIR_SET;
+typedef unordered_map<pair<int, int>, int, PairHash> PAIR_MAP;
