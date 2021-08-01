b59dce5028715d2a00b59c53302d49c8fcb16d71
jack <jack@mac_mini>
2021-08-01 19:20:55 +0800

    72. Edit Distance
    if word1[i-1] == word2[j-1]
      dp[i][j] = dp[i-1][j-1]
    else
      dp[i][j] = 1+min(dp[i-1][j-1], //replace
                       dp[i-1][j], // remove from word1
                       dp[i][j-1]) // insert at word1
    
    return dp[m][n]

--- leetcode/72.cpp
+++ leetcode/72.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        dp[0][0] = 0;
         for(int i = 1; i <= m; ++i) {
             dp[i][0] = i;
         }
-
         for(int j = 1; j <= n; ++j) {
             dp[0][j] = j;
         }
-
         for(int i = 1; i <= m; ++i) {
-            char &c1 = word1[i-1];
             for(int j = 1; j <= n; ++j) {
-                char &c2 = word2[j-1];
-                if(c1 == c2) {
+                if(word1[i-1] == word2[j-1]) {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else {
-                    // insert: as if w2[j] doesn't exist dp[i][j-1]
-                    // delete: as if w1[i] doesn't exist dp[i-1][j]
-                    // replace dp[i-1][j-1]
-                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+                    dp[i][j] = min(dp[i][j], dp[i-1][j]+1); // remove
+                    dp[i][j] = min(dp[i][j], dp[i][j-1]+1); // insert
+                    dp[i][j] = min(dp[i][j], dp[i-1][j-1]+1); // replace
                 }
             }
         }
