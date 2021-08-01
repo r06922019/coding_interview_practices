dcda44b715413b684b6fc0329b3d0c9e43ea7a85
jack <jack@mac_mini>
2021-08-01 22:56:53 +0800

    315
    Count reverse pairs
    
    merge sort!

--- leetcode/315.cpp
+++ leetcode/315.cpp
@@ -1,59 +1,39 @@
-class Item {
-public:
-    int index, value;
-    Item(int _i, int _v) {
-        index = _i;
-        value = _v;
-    }
-
-    Item(Item &&item) {
-        index = move(item.index);
-        value = move(item.value);
-    }
-
-    Item& operator=(Item&& other) {
-        index = move(other.index);
-        value = move(other.value);
-        return *this;
-    }
-};
-
 class Solution {
 public:
-    void merge_sort(vector<Item> &items, int start, int end, vector<int> &ans) {
-        if(end - start <= 1) return;
+    void merge_sort(vector<int> &nums, vector<int> &indexs, int start, int end, vector<int> &ans) {
+        if(end-start <= 1) return ;
 
         int mid = start + ((end-start) >> 1);
-        merge_sort(items, start, mid, ans);
-        merge_sort(items, mid, end, ans);
+        merge_sort(nums, indexs, start, mid, ans);
+        merge_sort(nums, indexs, mid, end, ans);
 
-        vector<Item> tmp;
-        tmp.reserve(end-start);
+        vector<int> tmp;
         int l = start, r = mid;
         while(l < mid || r < end) {
-            if(r >= end || (l < mid && items[l].value <= items[r].value)) {
-                ans[items[l].index] += r - mid;
-                tmp.push_back(move(items[l]));
+            if(r >= end || (l < mid && nums[indexs[l]] <= nums[indexs[r]])) {
+                ans[indexs[l]] += r-mid;
+                tmp.push_back(indexs[l]);
                 ++l;
             }
             else {
-                tmp.push_back(move(items[r]));
+                tmp.push_back(indexs[r]);
                 ++r;
             }
         }
         for(int i = start; i < end; ++i) {
-            items[i] = move(tmp[i-start]);
+            indexs[i] = tmp[i-start];
         }
         return;
     }
 
     vector<int> countSmaller(vector<int>& nums) {
-        vector<Item> items;
-        for(int i = 0; i < nums.size(); ++i) {
-            items.emplace_back(i, nums[i]);
+        int n = nums.size();
+        vector<int> indexs(n, 0);
+        for(int i = 0; i < n; ++i) {
+            indexs[i] = i;
         }
-        vector<int> ans(nums.size(), 0);
-        merge_sort(items, 0, items.size(), ans);
+        vector<int> ans(n, 0);
+        merge_sort(nums, indexs, 0, n, ans);
         return ans;
     }
 };
