7e1ca2cb19760d5fd097b8e3e760f605ac431527
jack <jack@mac_mini>
2022-03-29 23:42:56 +0800

    added another solution

--- leetcode/287.cpp
+++ leetcode/287.cpp
@@ -14,3 +14,16 @@ public:
         return slow;
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i) {
+            int num = abs(nums[i]);
+            if(nums[num] < 0) return num;
+            nums[num] *= -1;
+        }
+        return -1;
+    }
+};
