2a017b1479a73acbf21830a83214323071748989
jack <jack@mac_mini>
2022-04-06 22:39:58 +0800

    need some math

--- leetcode/923.cpp
+++ leetcode/923.cpp
@@ -1,80 +1,90 @@
-class Solution {
+class Solution
+{
 public:
-    int threeSumMulti(vector<int>& A, int target) {
-        int MOD = 1000000000+7;
-        vector<int> us_A = unique_and_sort(A);
-        unordered_map<int, int> counter_A = make_counter(A);
-        int N = us_A.size();
+    const int MOD = 1e9 + 7;
+
+    int mod(int x)
+    {
+        return x % MOD;
+    }
+
+    int mod(long long x)
+    {
+        return x % MOD;
+    }
+
+    int add(int a, int b)
+    {
+        return mod(mod(a) + mod(b));
+    }
+
+    int mul(int a, int b)
+    {
+        long long tmp = mod(a);
+        tmp *= mod(b);
+        return mod(tmp);
+    }
+
+    int threeSumMulti(vector<int> &arr, int target)
+    {
         int ans = 0;
-        for(int i = 0; i < N; ++i) {
-            int &num_i = us_A[i];
-            if( num_i * 3 == target) {
-                if(counter_A[ num_i ] >= 3)
-                    ans = (ans + C_n_get_k(counter_A[ num_i ], 3, MOD)) % MOD;
-            }
-            else if( num_i * 2 <= target) {
-                if(counter_A[ num_i ] >= 2) {
-                    int remainder = target - num_i * 2;
-                    long long cur_cnt = C_n_get_k(counter_A[ num_i ], 2, MOD);
-                    cur_cnt *= counter_A[remainder];
-                    ans = (ans + (cur_cnt % MOD)) % MOD;
+        vector<int> counter(100 + 1, 0);
+        for (int x : arr)
+        {
+            ++counter[x];
+        }
+
+        for (int j = 0; j <= 100; ++j)
+        {
+            if (counter[j] == 0)
+                continue;
+
+            //  1 j,  (i, j ,k)
+            for (int i = 0; i < j; ++i)
+            {
+                const int k = target - i - j;
+                if (i < j && j < k && k >= 0 && k < counter.size())
+                {
+                    ans = add(ans, mul(mul(counter[i], counter[j]), counter[k]));
                 }
             }
 
-            // 1
-            if(i < N-2) {
-                int l = i+1, r = N-1;
-                while(l < r) {
-                    int cur_sum = us_A[i] + us_A[l] + us_A[r];
-                    if(cur_sum == target) {
-                        long long cur_cnt = 1;
-                        cur_cnt = cur_cnt * counter_A[ us_A[i] ];
-                        cur_cnt = cur_cnt * counter_A[ us_A[l] ];
-                        cur_cnt = cur_cnt * counter_A[ us_A[r] ];
-                        ans = (ans + (cur_cnt % MOD)) % MOD;
-                    }
-
-                    if(cur_sum > target) {
-                        --r;
-                    }
-                    else {
-                        ++l;
-                    }
+            // 2 js
+            if (target >= 2 * j)
+            {
+                const long long cur = counter[j] * (counter[j] - 1) / 2;
+                const int x = target - 2 * j;
+                if (x != j && x < counter.size())
+                {
+                    ans += mul(mod(cur), counter[x]);
                 }
+            }
+
+            // 3j
+            if (target == 3 * j)
+            {
+                const long long cur = (long long)counter[j] * (counter[j] - 1) * (counter[j] - 2);
+                ans += mod(cur / 6);
             }
         }
         return ans;
     }
-
-    vector<int> unique_and_sort(vector<int> &A) {
-        set<int> set_a(A.begin(), A.end());
-        return vector<int>(set_a.begin(), set_a.end());
-    }
-
-    unordered_map<int, int> make_counter(vector<int> &A) {
-        unordered_map<int, int> counter_A;
-        for(int &n : A) {
-            ++counter_A[n];
-        }
-        return counter_A;
-    }
-
-    int C_n_get_k(int n, int k, int MOD) {
-        if(k > n/2) k = n-k;
-        long long ans = 1;
-        for(int i = 1; i <= k; ++i) {
-            if(ans % i == 0) {
-                ans = ans / i;
-                ans = ans * (n - i + 1);
+    /*
+    int threeSumMulti(vector<int>& arr, int target) {
+        int ans = 0, n = arr.size();
+        for(int i = 0; i < n; ++i) {
+            int cur = 0;
+            vector<int> counter(100+1, 0);
+            for(int j = i+1; j < n; ++j) {
+                int k = target-arr[i]-arr[j];
+                if(k >= 0 && k < counter.size()) {
+                    cur = add(cur, counter[k]);
+                }
+                ++counter[arr[j]];
             }
-            else {
-                ans = ans * (n - i + 1);
-                ans = ans % MOD;
-                ans = ans / i;
-            }
-            ans = ans % MOD;
+            ans = add(ans, cur);
         }
         return ans;
     }
-
+    */
 };
