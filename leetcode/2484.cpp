d2b0cafc176c8d81569a7263b4f25d2c0b33ebc7
jack <jack@mac_mini>
2022-11-27 01:04:19 +0800

    Seems like n^2 prefixes but actually only 100
    for all indexes, find possible prefixes(100) and suffixes(100)
    total complexity is 100n

---
+++ leetcode/2484.cpp
@@ -0,0 +1,67 @@
+class Solution
+{
+public:
+    const int MOD = 1e9 + 7;
+    int mod(const int &x)
+    {
+        return x % MOD;
+    }
+    int mod(const long long &x)
+    {
+        return x % MOD;
+    }
+    int add(const int &a, const int &b)
+    {
+        return mod(mod(a) + mod(b));
+    }
+    int mul(const int &a, const int &b)
+    {
+        long long tmp = mod(a);
+        tmp *= mod(b);
+        return mod(tmp);
+    }
+    void fill_in(vector<vector<int>> &prefix, string s, bool rrr)
+    {
+        vector<int> counts(10, 0);
+        int n = s.size();
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int digit = s[i] - '0';
+            if (i >= 1)
+            {
+                prefix[i + 1] = prefix[i];
+                for (int a = 0; a <= 9; ++a)
+                {
+                    const int num = (rrr) ? digit * 10 + a : a * 10 + digit;
+                    prefix[i + 1][num] = add(prefix[i + 1][num], counts[a]);
+                }
+            }
+            ++counts[digit];
+        }
+        if (rrr)
+        {
+            reverse(prefix.begin(), prefix.end());
+        }
+    }
+    int countPalindromes(string s)
+    {
+        if (s.size() < 5)
+            return 0;
+        vector<vector<int>> prefix(s.size(), vector<int>(100, 0)), suffix(s.size(), vector<int>(100, 0));
+        fill_in(prefix, s, false);
+        fill_in(suffix, string(s.rbegin(), s.rend()), true);
+        int ans = 0;
+        for (int i = 2; i < s.size() - 2; ++i)
+        {
+            for (int a = 0; a <= 9; ++a)
+            {
+                for (int b = 0; b <= 9; ++b)
+                {
+                    const int x = a * 10 + b, y = b * 10 + a;
+                    ans = add(ans, mul(prefix[i][x], suffix[i][y]));
+                }
+            }
+        }
+        return ans;
+    }
+};
